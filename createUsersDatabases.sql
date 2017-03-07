82e2d52ab8a84263584cc6ec53b5b515947e8c4c
Gijs Wiggers <info@gijs.tech>
2017-03-07 11:53:01 +0100

    Cleanup and adding world.sql

--- createUsersDatabases.sql
+++ createUsersDatabases.sql
@@ -1,150 +1,299 @@
-CREATE USER 'user1'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 CREATE DATABASE Database1;
+USE Database1;
+SOURCE world.sql;
+CREATE USER 'user1'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database1 . * TO 'user1'@'localhost';
-CREATE USER 'user2'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database2;
+USE Database2;
+SOURCE world.sql;
+CREATE USER 'user2'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database2 . * TO 'user2'@'localhost';
-CREATE USER 'user3'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database3;
+USE Database3;
+SOURCE world.sql;
+CREATE USER 'user3'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database3 . * TO 'user3'@'localhost';
-CREATE USER 'user4'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database4;
+USE Database4;
+SOURCE world.sql;
+CREATE USER 'user4'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database4 . * TO 'user4'@'localhost';
-CREATE USER 'user5'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database5;
+USE Database5;
+SOURCE world.sql;
+CREATE USER 'user5'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database5 . * TO 'user5'@'localhost';
-CREATE USER 'user6'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database6;
+USE Database6;
+SOURCE world.sql;
+CREATE USER 'user6'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database6 . * TO 'user6'@'localhost';
-CREATE USER 'user7'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database7;
+USE Database7;
+SOURCE world.sql;
+CREATE USER 'user7'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database7 . * TO 'user7'@'localhost';
-CREATE USER 'user8'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database8;
+USE Database8;
+SOURCE world.sql;
+CREATE USER 'user8'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database8 . * TO 'user8'@'localhost';
-CREATE USER 'user9'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database9;
+USE Database9;
+SOURCE world.sql;
+CREATE USER 'user9'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database9 . * TO 'user9'@'localhost';
-CREATE USER 'user10'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database10;
+USE Database10;
+SOURCE world.sql;
+CREATE USER 'user10'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database10 . * TO 'user10'@'localhost';
-CREATE USER 'user11'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database11;
+USE Database11;
+SOURCE world.sql;
+CREATE USER 'user11'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database11 . * TO 'user11'@'localhost';
-CREATE USER 'user12'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database12;
+USE Database12;
+SOURCE world.sql;
+CREATE USER 'user12'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database12 . * TO 'user12'@'localhost';
-CREATE USER 'user13'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database13;
+USE Database13;
+SOURCE world.sql;
+CREATE USER 'user13'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database13 . * TO 'user13'@'localhost';
-CREATE USER 'user14'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database14;
+USE Database14;
+SOURCE world.sql;
+CREATE USER 'user14'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database14 . * TO 'user14'@'localhost';
-CREATE USER 'user15'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database15;
+USE Database15;
+SOURCE world.sql;
+CREATE USER 'user15'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database15 . * TO 'user15'@'localhost';
-CREATE USER 'user16'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database16;
+USE Database16;
+SOURCE world.sql;
+CREATE USER 'user16'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database16 . * TO 'user16'@'localhost';
-CREATE USER 'user17'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database17;
+USE Database17;
+SOURCE world.sql;
+CREATE USER 'user17'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database17 . * TO 'user17'@'localhost';
-CREATE USER 'user18'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database18;
+USE Database18;
+SOURCE world.sql;
+CREATE USER 'user18'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database18 . * TO 'user18'@'localhost';
-CREATE USER 'user19'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database19;
+USE Database19;
+SOURCE world.sql;
+CREATE USER 'user19'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database19 . * TO 'user19'@'localhost';
-CREATE USER 'user20'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database20;
+USE Database20;
+SOURCE world.sql;
+CREATE USER 'user20'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database20 . * TO 'user20'@'localhost';
-CREATE USER 'user21'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database21;
+USE Database21;
+SOURCE world.sql;
+CREATE USER 'user21'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database21 . * TO 'user21'@'localhost';
-CREATE USER 'user22'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database22;
+USE Database22;
+SOURCE world.sql;
+CREATE USER 'user22'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database22 . * TO 'user22'@'localhost';
-CREATE USER 'user23'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database23;
+USE Database23;
+SOURCE world.sql;
+CREATE USER 'user23'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database23 . * TO 'user23'@'localhost';
-CREATE USER 'user24'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database24;
+USE Database24;
+SOURCE world.sql;
+CREATE USER 'user24'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database24 . * TO 'user24'@'localhost';
-CREATE USER 'user25'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database25;
+USE Database25;
+SOURCE world.sql;
+CREATE USER 'user25'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database25 . * TO 'user25'@'localhost';
-CREATE USER 'user26'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database26;
+USE Database26;
+SOURCE world.sql;
+CREATE USER 'user26'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database26 . * TO 'user26'@'localhost';
-CREATE USER 'user27'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database27;
+USE Database27;
+SOURCE world.sql;
+CREATE USER 'user27'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database27 . * TO 'user27'@'localhost';
-CREATE USER 'user28'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database28;
+USE Database28;
+SOURCE world.sql;
+CREATE USER 'user28'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database28 . * TO 'user28'@'localhost';
-CREATE USER 'user29'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database29;
+USE Database29;
+SOURCE world.sql;
+CREATE USER 'user29'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database29 . * TO 'user29'@'localhost';
-CREATE USER 'user30'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database30;
+USE Database30;
+SOURCE world.sql;
+CREATE USER 'user30'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database30 . * TO 'user30'@'localhost';
-CREATE USER 'user31'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database31;
+USE Database31;
+SOURCE world.sql;
+CREATE USER 'user31'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database31 . * TO 'user31'@'localhost';
-CREATE USER 'user32'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database32;
+USE Database32;
+SOURCE world.sql;
+CREATE USER 'user32'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database32 . * TO 'user32'@'localhost';
-CREATE USER 'user33'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database33;
+USE Database33;
+SOURCE world.sql;
+CREATE USER 'user33'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database33 . * TO 'user33'@'localhost';
-CREATE USER 'user34'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database34;
+USE Database34;
+SOURCE world.sql;
+CREATE USER 'user34'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database34 . * TO 'user34'@'localhost';
-CREATE USER 'user35'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database35;
+USE Database35;
+SOURCE world.sql;
+CREATE USER 'user35'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database35 . * TO 'user35'@'localhost';
-CREATE USER 'user36'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database36;
+USE Database36;
+SOURCE world.sql;
+CREATE USER 'user36'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database36 . * TO 'user36'@'localhost';
-CREATE USER 'user37'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database37;
+USE Database37;
+SOURCE world.sql;
+CREATE USER 'user37'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database37 . * TO 'user37'@'localhost';
-CREATE USER 'user38'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database38;
+USE Database38;
+SOURCE world.sql;
+CREATE USER 'user38'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database38 . * TO 'user38'@'localhost';
-CREATE USER 'user39'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database39;
+USE Database39;
+SOURCE world.sql;
+CREATE USER 'user39'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database39 . * TO 'user39'@'localhost';
-CREATE USER 'user40'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database40;
+USE Database40;
+SOURCE world.sql;
+CREATE USER 'user40'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database40 . * TO 'user40'@'localhost';
-CREATE USER 'user41'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database41;
+USE Database41;
+SOURCE world.sql;
+CREATE USER 'user41'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database41 . * TO 'user41'@'localhost';
-CREATE USER 'user42'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database42;
+USE Database42;
+SOURCE world.sql;
+CREATE USER 'user42'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database42 . * TO 'user42'@'localhost';
-CREATE USER 'user43'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database43;
+USE Database43;
+SOURCE world.sql;
+CREATE USER 'user43'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database43 . * TO 'user43'@'localhost';
-CREATE USER 'user44'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database44;
+USE Database44;
+SOURCE world.sql;
+CREATE USER 'user44'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database44 . * TO 'user44'@'localhost';
-CREATE USER 'user45'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database45;
+USE Database45;
+SOURCE world.sql;
+CREATE USER 'user45'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database45 . * TO 'user45'@'localhost';
-CREATE USER 'user46'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database46;
+USE Database46;
+SOURCE world.sql;
+CREATE USER 'user46'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database46 . * TO 'user46'@'localhost';
-CREATE USER 'user47'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database47;
+USE Database47;
+SOURCE world.sql;
+CREATE USER 'user47'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database47 . * TO 'user47'@'localhost';
-CREATE USER 'user48'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database48;
+USE Database48;
+SOURCE world.sql;
+CREATE USER 'user48'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database48 . * TO 'user48'@'localhost';
-CREATE USER 'user49'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database49;
+USE Database49;
+SOURCE world.sql;
+CREATE USER 'user49'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database49 . * TO 'user49'@'localhost';
-CREATE USER 'user50'@'localhost' IDENTIFIED BY 'P@ssw0rd';
+
 CREATE DATABASE Database50;
+USE Database50;
+SOURCE world.sql;
+CREATE USER 'user50'@'localhost' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON Database50 . * TO 'user50'@'localhost';
